d369c5d4c0d83582cf6863c8d1e2392bbb1638a7
HuJJJJ <1602397209@qq.com>
2023-12-09 00:15:12 +0800

    修改request。 新增解包器功能

--- ApeFree.Protocol.ApeFtp/DemandRequest.cs
+++ ApeFree.Protocol.ApeFtp/DemandRequest.cs
@@ -9,24 +9,23 @@ namespace ApeFree.Protocol.ApeFtp
     /// </summary>
     public class DemandRequest : BaseRequest
     {
-        public IEnumerable<byte> MD5 { get; set; }
-        public uint Length { get; set; }
         public uint SegmentMaxLength { get; set; }
 
-        public DemandRequest() : base(CommandCode.DemandRequest) { }
-        public DemandRequest(IEnumerable<byte> data) : base(CommandCode.DemandRequest)
+        public DemandRequest(byte[] data) : base(CommandCode.DemandRequest, data.Skip(1).Take(16).ToArray(), BitConverter.ToUInt32(data.Skip(17).Take(4).Reverse().ToArray(), 0))
         {
-            List<byte> bytes = data.ToList();
-            MD5 = bytes.Skip(1).Take(16);
-            Length = BitConverter.ToUInt32(bytes.Skip(17).Take(4).Reverse().ToArray(), 0);
-            SegmentMaxLength = BitConverter.ToUInt32(bytes.Skip(21).Take(4).Reverse().ToArray(), 0);
+            SegmentMaxLength = BitConverter.ToUInt32(data.Skip(21).Take(4).Reverse().ToArray(), 0);
+        }
+
+        public DemandRequest(byte[] mD5, uint totalLength, uint segmentMaxLength) : base(CommandCode.DemandRequest, mD5, totalLength)
+        {
+            SegmentMaxLength = segmentMaxLength;
         }
 
         public override byte[] GetBytes()
         {
             return new byte[] { (byte)CommandCode }.Merge(
                                     MD5,
-                                    BitConverter.GetBytes(Length).Reverse(),
+                                    BitConverter.GetBytes(TotalLength).Reverse(),
                                     BitConverter.GetBytes(SegmentMaxLength).Reverse()
                                 ).ToArray();
         }
--- ApeFree.Protocol.ApeFtp/TransferResponse.cs
+++ ApeFree.Protocol.ApeFtp/TransferResponse.cs
@@ -1,36 +1,42 @@
-﻿using System;
+﻿using STTech.BytesIO.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace ApeFree.Protocol.ApeFtp
 {
     /// <summary>
     /// 传输响应
     /// </summary>
-    public class TransferResponse : BaseRequest
+    public class TransferResponse : Response
     {
-        public IEnumerable<byte> MD5 { get; set; }
+        public CommandCode CommandCode { get; } = CommandCode.TransferResponse;
+
+        public uint TotalLength { get; set; }
+        public byte[] MD5 { get; set; }
         public ResultCode ResultCode { get; set; }
         public byte MessageLength => (byte)Message.Length;
         public string Message { get; set; } = "";
 
-        public TransferResponse() : base(CommandCode.TransferResponse) { }
-        public TransferResponse(IEnumerable<byte> bytes) : base(CommandCode.TransferResponse)
+
+        public TransferResponse(byte[] bytes) : base(bytes)
         {
-            MD5 = bytes.Skip(1).Take(16);
-            ResultCode = (ResultCode)bytes.ElementAt(17);
-            var messageLength = bytes.ElementAt(18);
-            Message = bytes.Skip(19).Take(messageLength).EncodeToString();
+            MD5 = bytes.Skip(1).Take(16).ToArray();
+            TotalLength = BitConverter.ToUInt32(bytes.Skip(17).Take(4).Reverse().ToArray(), 0);
+            ResultCode = (ResultCode)bytes.ElementAt(22);
+            var messageLength = bytes.ElementAt(23);
+            Message = bytes.Skip(23).Take(messageLength).EncodeToString();
         }
 
-        public override byte[] GetBytes()
+        public byte[] GetBytes()
         {
             return new byte[] { (byte)CommandCode }.Merge(
                                     MD5,
+                                    BitConverter.GetBytes(TotalLength).Reverse(),
                                     new byte[] { (byte)ResultCode },
                                     new byte[] { MessageLength },
-                                    Message.GetBytes()
-                                ).ToArray();
+                                    Message.GetBytes()).ToArray();
         }
     }
 }
