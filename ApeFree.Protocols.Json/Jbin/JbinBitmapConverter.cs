9b5b698c866eafe062e2b4b1063fc56a476b05c5
Guijie Lee <landriesnidis@yeah.net>
2024-09-18 11:01:00 +0800

    修改Bitmap的序列化方法

--- ApeFree.Protocols.Json/Jbin/JbinBitmapConverter.cs
+++ ApeFree.Protocols.Json/Jbin/JbinBitmapConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
@@ -13,44 +14,55 @@ namespace ApeFree.Protocols.Json.Jbin
     {
         protected override Bitmap ConvertBytesToValue(byte[] bytes, Type objectType)
         {
-            var w = BitConverter.ToInt32(bytes, 0);
-            var h = BitConverter.ToInt32(bytes, 4);
-            var f = (PixelFormat)BitConverter.ToInt32(bytes, 8);
+            //var w = BitConverter.ToInt32(bytes, 0);
+            //var h = BitConverter.ToInt32(bytes, 4);
+            //var f = (PixelFormat)BitConverter.ToInt32(bytes, 8);
 
-            Bitmap bitmap = new Bitmap(w, h, f);
+            //Bitmap bitmap = new Bitmap(w, h, f);
 
-            // 锁定图像的像素数据
-            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
-            BitmapData bitmapData = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+            //// 锁定图像的像素数据
+            //Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            //BitmapData bitmapData = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
 
-            try
+            //try
+            //{
+            //    // 获取指向像素数据的指针
+            //    IntPtr ptr = bitmapData.Scan0;
+
+            //    // 从指针处复制数据到我们的字节数组
+            //    Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            //}
+            //finally
+            //{
+            //    // 释放像素数据的锁定
+            //    bitmap.UnlockBits(bitmapData);
+            //}
+
+            //return bitmap;
+
+            using (MemoryStream ms = new MemoryStream(bytes))
             {
-                // 获取指向像素数据的指针
-                IntPtr ptr = bitmapData.Scan0;
-
-                // 从指针处复制数据到我们的字节数组
-                Marshal.Copy(bytes, 0, ptr, bytes.Length);
+                return (Bitmap)Image.FromStream(ms);
             }
-            finally
-            {
-                // 释放像素数据的锁定
-                bitmap.UnlockBits(bitmapData);
-            }
-
-            return bitmap;
         }
 
         protected override byte[] ConvertValueToBytes(Bitmap bitmap)
         {
-            List<byte[]> lst = new List<byte[]>();
+            //List<byte[]> lst = new List<byte[]>();
 
-            lst.Add(BitConverter.GetBytes(bitmap.Width));
-            lst.Add(BitConverter.GetBytes(bitmap.Height));
-            lst.Add(BitConverter.GetBytes((int)bitmap.PixelFormat));
-            lst.Add(GetBitmapDataBytes(bitmap));
+            //lst.Add(BitConverter.GetBytes(bitmap.Width));
+            //lst.Add(BitConverter.GetBytes(bitmap.Height));
+            //lst.Add(BitConverter.GetBytes((int)bitmap.PixelFormat));
+            //lst.Add(GetBitmapDataBytes(bitmap));
 
-            var bytes = JbinObject.ConcatByteArrays(lst);
-            return bytes;
+            //var bytes = JbinObject.ConcatByteArrays(lst);
+            //return bytes;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png); // 可以根据需要选择图像格式，如 Jpeg、Bmp 等
+                return ms.ToArray();
+            }
         }
 
         public byte[] GetBitmapDataBytes(Bitmap bitmap)
