ea47b83ba12ccd793e0274fd7c60944edd2bfcac
Guijie Lee <landriesnidis@yeah.net>
2025-08-27 11:47:39 +0800

    新增大数据序列化测试文件及项目配置更新
    在测试项目中包含 `JbinBigDataTest.cs` 文件。
    新增 `JbinBigDataTest.cs` 测试文件：
    - 定义 `JbinBigDataTest` 测试类，包含序列化和反序列化测试方法。
    - 添加 `TestBigData` 类，用于存储大数据对象。
    更新项目文件以包含新测试文件。

---
+++ ApeFree.Protocols.Json.Tests/JbinBigDataTest.cs
@@ -0,0 +1,58 @@
+﻿using ApeFree.Protocols.Json.Jbin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApeFree.Protocols.Json.Tests
+{
+    [TestClass]
+    public class JbinBigDataTest
+    {
+        [TestMethod]
+        public void JbinBigDataSerializeTest()
+        {
+            var obj = new TestBigData()
+            {
+                Data1 = new byte[1024 * 1024 * 1024],
+                Data2 = new byte[1024 * 1024 * 1024],
+            };
+            var jbin = JbinObject.FromObject(obj);
+
+            using (var stream = File.OpenWrite("testbigdata.jbin"))
+            {
+                jbin.WriteTo(stream);
+            }
+        }
+
+        [TestMethod]
+        public void JbinBigDataDeserializeTest()
+        {
+            using (var stream = File.OpenRead("testbigdata.jbin"))
+            {
+                var jbin = JbinObject.Parse(stream);
+                var obj = jbin.ToObject<TestBigData>();
+
+                Assert.IsNotNull(obj);
+                Assert.IsNotNull(obj.Data1);
+                Assert.IsNotNull(obj.Data2);
+                Assert.AreEqual(1024 * 1024 * 1024, obj.Data1.Length);
+                Assert.AreEqual(1024 * 1024 * 1024, obj.Data2.Length);
+            }
+        }
+    }
+
+    public class TestBigData
+    {
+        public TestBigData()
+        {
+        }
+
+        public byte[] Data1 { get; set; }
+        public byte[] Data2 { get; set; }
+    }
+}
