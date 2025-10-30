bdb442dcb53ac7d6ca63e116d8da92189cd7e92e
Guijie Lee <landriesnidis@yeah.net>
2025-10-30 14:26:29 +0800

    ✨ feat(jbin-rpc): 添加 JbinRpcReflector 反射器实现
    - 新增 JbinRpcReflector 类，支持通过 Jbin 格式进行 JsonRPC 方法调用和事件触发
    - 实现 ReflectInvokeMethod 方法，处理 Jbin 格式的请求和响应数据转换
    - 实现 ReflectRaiseEvent 方法，支持基于 Jbin 格式的事件反射机制
    
    ✨ feat(json-rpc): 完善 JsonRpcReflector 反射器功能
    
    - 为 JsonRpcReflector 添加完整的 JsonRPC 协议支持
    - 实现多参数类型的 ReflectInvokeMethod 方法重载，支持字节数组和字符串格式的请求
    - 添加完善的错误处理机制，包括反序列化异常、方法未找到和内部错误处理
    - 实现 ReflectRaiseEvent 方法，支持事件触发功能
    - 配置 JsonSerializerSettings 序列化设置，支持类型处理和空值忽略

---
+++ ApeFree.Protocols.Json/Jbin/Reflectors/JbinRpcReflector.cs
@@ -0,0 +1,54 @@
+﻿using ApeFree.Protocols.Json.Jbin;
+using ApeFree.Protocols.Json.JsonRpc;
+using ApeFree.Protocols.Json.JsonRpc.Reflectors;
+using STTech.CodePlus.Components;
+using System;
+
+namespace ApeFree.Protocols.Json.Jbin.Reflectors
+{
+    /// <summary>
+    /// JbinRpc反射器
+    /// </summary>
+    public class JbinRpcReflector : Reflector
+    {
+        /// <inheritdoc/>
+        public override byte[] ReflectInvokeMethod(object reflectObject, byte[] bytes)
+        {
+            // 解析JsonRpc请求数据包到 Jbin 对象
+            var jbinReq = JbinObject.Parse(bytes);
+
+            // 由 Jbin 对象反序列化为JsonRpc请求对象
+            var req = jbinReq.ToObject<JsonRpcRequest>();
+
+            // 执行JsonRpc的反射，得到返回的JsonRpc响应对象
+            var resp = JsonRpcReflector.ReflectInvokeMethod(reflectObject, req);
+
+            // 将JsonRpc响应对象序列化成为 Jbin 对象
+            var jbinResp = JbinObject.FromObject(resp);
+
+            // 将Jbin格式的响应数据转为字节数组
+            var respBytes = jbinResp.ToBytes();
+
+            // 如果返回结果是可销毁的对象，则执行销毁方法
+            if (resp.Result is IDisposable o)
+            {
+                o.Dispose();
+            }
+
+            return respBytes;
+        }
+
+        /// <inheritdoc/>
+        public override void ReflectRaiseEvent(object reflectObject, byte[] bytes)
+        {
+            // 解析JsonRpc请求数据包到 Jbin 对象
+            var jbinReq = JbinObject.Parse(bytes);
+
+            // 由 Jbin 对象反序列化为JsonRpc请求对象
+            var req = jbinReq.ToObject<JsonRpcRequest>();
+
+            // 执行JsonRpc的反射，得到返回的JsonRpc响应对象
+            JsonRpcReflector.ReflectRaiseEvent(reflectObject, req);
+        }
+    }
+}
---
+++ ApeFree.Protocols.Json/JsonRpc/Reflectors/JsonRpcReflector.cs
@@ -0,0 +1,188 @@
+﻿using Newtonsoft.Json;
+using STTech.CodePlus.Components;
+using STTech.CodePlus.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApeFree.Protocols.Json.JsonRpc.Reflectors
+{
+    /// <summary>
+    /// JsonRPC反射器
+    /// </summary>
+    public class JsonRpcReflector : Reflector
+    {
+        /// <summary>
+        /// Json序列化设置
+        /// </summary>
+        public readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
+        {
+            TypeNameHandling = TypeNameHandling.All,
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        #region 反射调用方法
+        /// <inheritdoc/>
+        public override byte[] ReflectInvokeMethod(object reflectObject, byte[] bytes)
+        {
+            // 调用内部方法进行反射处理，并将结果转换为字符串
+            var jsonResp = ReflectInvokeMethod(reflectObject, bytes.EncodeToString());
+            // 将字符串转换为字节数组返回
+            var respBytes = jsonResp.GetBytes();
+            return respBytes;
+        }
+
+        /// <summary>
+        /// JsonRPC反射调用方法
+        /// </summary>
+        /// <param name="reflectObject">对象</param>
+        /// <param name="reqJson">请求的Json字符串</param>
+        /// <returns>字符串</returns>
+        public string ReflectInvokeMethod(object reflectObject, string reqJson)
+        {
+            JsonRpcRequest req = null;
+
+            try
+            {
+                // 尝试反序列化请求的Json字符串为JsonRpcRequest对象
+                req = JsonRpcExtensions.DeserializeRequest(reqJson);
+            }
+            catch (Exception ex)
+            {
+                // 如果反序列化失败，返回包含错误信息的JsonRpcResponse的Json字符串
+                return new JsonRpcResponse()
+                {
+                    JsonRpc = req.JsonRpc,
+                    Id = req.Id,
+                    Result = null,
+                    Error = new JsonRpcError()
+                    {
+                        Code = JsonRpcErrorCode.ParseError,
+                        Message = ex.Message,
+                    }
+                }.ToJsonString();
+            }
+
+            var resp = ReflectInvokeMethod(reflectObject, req);
+            var respJson = resp.ToJsonString();
+
+            if (resp.Result is IDisposable o)
+            {
+                // 如果结果可释放，进行释放操作
+                o.Dispose();
+            }
+
+            return respJson;
+        }
+
+        /// <summary>
+        /// 内部的JsonRPC反射调用方法，处理对象和JsonRpcRequest对象
+        /// </summary>
+        /// <param name="reflectObject">反射的目标对象</param>
+        /// <param name="req">JsonRpcRequest对象</param>
+        /// <returns>JsonRpcResponse对象</returns>
+        public static JsonRpcResponse ReflectInvokeMethod(object reflectObject, JsonRpcRequest req)
+        {
+            var mi = ReflectionUtils.MatchMethod(reflectObject, req.Method, req.Params);
+
+            if (mi == null)
+            {
+                // 如果未找到匹配的方法，返回包含错误信息的JsonRpcResponse对象
+                return new JsonRpcResponse()
+                {
+                    JsonRpc = req.JsonRpc,
+                    Id = req.Id,
+                    Result = null,
+                    Error = new JsonRpcError()
+                    {
+                        Code = JsonRpcErrorCode.MethodNotFound,
+                        Message = $"{nameof(JsonRpcErrorCode.MethodNotFound)}({req.Method})",
+                    }
+                };
+            }
+
+            try
+            {
+                // 转换方法的参数类型
+                req.Params = ReflectionUtils.ConvertMethodParameterType(mi, req.Params);
+
+                // 调用方法并获取结果
+                object result = mi.Invoke(reflectObject, req.Params);
+
+                // 返回包含结果的JsonRpcResponse对象
+                return new JsonRpcResponse()
+                {
+                    JsonRpc = req.JsonRpc,
+                    Id = req.Id,
+                    Result = result,
+                    Error = null,
+                };
+            }
+            catch (Exception ex)
+            {
+                List<string> errorMessageList = new List<string>();
+                while (ex != null)
+                {
+                    errorMessageList.Add(ex.Message);
+                    ex = ex.InnerException;
+                }
+
+                // 如果调用方法时出错，返回包含错误信息的JsonRpcResponse对象
+                return new JsonRpcResponse()
+                {
+                    JsonRpc = req.JsonRpc,
+                    Id = req.Id,
+                    Result = null,
+                    Error = new JsonRpcError()
+                    {
+                        Code = JsonRpcErrorCode.InternalError,
+                        Message = errorMessageList.Join("\r\n"),
+                    }
+                };
+            }
+        }
+        #endregion
+
+
+        #region 反射触发事件
+        /// <inheritdoc/>
+        public override void ReflectRaiseEvent(object reflectObject, byte[] bytes)
+        {
+            // 调用内部方法进行反射处理
+            ReflectRaiseEvent(reflectObject, bytes.EncodeToString());
+        }
+
+        /// <summary>
+        /// 内部的JsonRPC反射触发事件的方法，处理对象和请求的Json字符串
+        /// </summary>
+        /// <param name="reflectObject">反射的目标对象</param>
+        /// <param name="reqJson">请求的Json字符串</param>
+        public void ReflectRaiseEvent(object reflectObject, string reqJson)
+        {
+            JsonRpcRequest req;
+            try
+            {
+                // 尝试反序列化请求的Json字符串为JsonRpcRequest对象
+                req = JsonRpcExtensions.DeserializeRequest(reqJson);
+            }
+            catch (Exception ex)
+            {
+                // 如果反序列化失败，抛出异常
+                throw new Exception("反序列化失败。", ex);
+            }
+
+            ReflectRaiseEvent(reflectObject, req);
+        }
+
+        /// <summary>
+        /// JsonRPC反射触发事件的方法
+        /// </summary>
+        /// <param name="reflectObject">反射的目标对象</param>
+        /// <param name="req">JsonRpcRequest对象</param>
+        public static void ReflectRaiseEvent(object reflectObject, JsonRpcRequest req)
+        {
+            ReflectionUtils.ReflectRaiseEvent(reflectObject, req.Method, req.Params);
+        }
+        #endregion
+    }
+}
